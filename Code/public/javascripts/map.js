836c96f8f47dc90ff2c5980a255ab9cf778027e7
Bernardo Antunes <55885986+BernardoAntunes22@users.noreply.github.com>
2021-05-01 00:37:19 +0100

    Icon map changes and MyRides added

--- Code/public/javascripts/map.js
+++ Code/public/javascripts/map.js
@@ -1,45 +1,49 @@
 var map;
 var marker;
 
-
 function map() {
-    map = L.map('map').setView([38.70736728020726, -9.152443430499575], 13);//Zoom to IADE
-    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=nzLBBO7atyb6b5uMflmX', {
-        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    }).addTo(map);
-    Iade()//Marker do IADE
- 
+  map = L.map("map").setView([38.70736728020726, -9.152443430499575], 13); //Zoom to IADE
+  L.tileLayer(
+    "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=nzLBBO7atyb6b5uMflmX",
+    {
+      attribution:
+        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    }
+  ).addTo(map);
+  
 }
-
 
 uniIcon = L.icon({
-    iconUrl: "images/BUni.png",
-    iconSize:     [25, 25], 
+  iconUrl: "images/BUni.png",
+  iconSize: [50, 50],
 });
 
-function Iade (){
-    marker = L.marker([38.70738402469949, -9.152389786320123], {icon: uniIcon}).addTo(map);
+rideIcon = L.icon({
+  iconUrl: "images/CarS.png",
+  iconSize:     [75, 50], 
+});
 
+function Iade() {
+  marker = L.marker([38.70738402469949, -9.152389786320123], {
+    icon: uniIcon,
+  }).addTo(map);
 }
 
-
-
-
-
-
-
+async function rideMarkers(){
+  rides = await $.ajax({
+      url:"/API/rides",
+      method: "get",
+  });
+  for (var idx in rides) {
+      let ride = rides[idx];
+      L.marker({lat: ride.R_lat, lon: ride.R_long}, {icon: rideIcon}).addTo(map);
+  }
+}
 
 
 
 window.onload = () => {
-
-    map();
-  
-
-}
-
-
-
-
-
-
+  map();
+  Iade()
+  rideMarkers();
+};
